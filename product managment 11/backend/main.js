2ca58fc1939aff206e2adbecc85d3b311c59ae4f
Imran_haider <ih5486613@gmail.com>
2026-04-27 16:07:43 +0500

    Reorder middleware and connection setup in main.js

--- product managment 11/backend/main.js
+++ product managment 11/backend/main.js
@@ -6,10 +6,10 @@ const app = express();
 const Port = 3000;
 import ProductRoutes from './src/Routes/ProductRoutes.js'
 import userRoutes from './src/Routes/UserRoute.js'
+app.use(cors()); 
 app.use(express.json())
 ConnectDB();
 // Corrected line: invoke the function
-app.use(cors()); 
 app.use('/products', ProductRoutes)
 app.use('/user', userRoutes)
 app.listen(Port, () => {
