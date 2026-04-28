2214ddd39fd4f217c77b9d950b0ec0d0ae1f8924
Imran_haider <ih5486613@gmail.com>
2026-04-28 11:02:32 +0500

    Delete product managment 11/backend directory

--- product managment 11/backend/src/Routes/UserRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createUser } from '../Controllers/User.Controller.js'
-
-const Router = express.Router()
-
-Router.get('/', createUser)
-
-
-export default Router
